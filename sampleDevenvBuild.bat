53eddda87e32c33e4d8969f5112dd1ac14468887
Kevin Berridge <kberridge@pointeblank.net>
2010-05-04 14:59:08 -0400

    Added sample build.bats, modifed snippets

---
+++ sampleDevenvBuild.bat
@@ -0,0 +1,5 @@
+REM Place this file next your .sln
+
+"C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" Sample.sln /Build Debug
+
+REM If you're using VS express the command is VCSExpress.exe
---
+++ sampleRakeBuild.bat
@@ -0,0 +1,4 @@
+REM Place this file next to your rakefile and update the parameters to the rake command as needed
+REM If you are using albacore do build your .sln file, your project files must contain <GenerateFullPaths>True</GenerateFullPaths> in the common PropertyGroup
+
+rake debug
